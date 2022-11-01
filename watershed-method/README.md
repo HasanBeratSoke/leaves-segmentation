d8573b906045b5bdf389d2a7646912a9151dd7ac
HasanBeratSoke <hasanberatsoke@hotmail.com>
2022-11-01 16:32:00 +0300

    added

---
+++ watershed-method/README.md
@@ -0,0 +1,19 @@
+## Watershed algoritması
+Matematiksel morfolojik tabanlı, bölge bazlı bir algoritmadır.
+
+Algoritma, üzerinde çalıştığı görüntüye, arkaplan ve önplan ayrımını yapmaya çalışır. 
+
+Yüksekliği temsil eden tepe noktaları takip eden çizgiler çizer.
+
+
+
+
+<p align="center">
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Relief_of_gradient_of_heart_MRI.png/180px-Relief_of_gradient_of_heart_MRI.png" width="200" height="200" />
+  <img src="https://upload.wikimedia.org/wikipedia/commons/0/0d/Watershed_of_gradient_of_MRI_heart_image.png" width="200" height="200" />
+</p>
+
+---
+
+#### Result
+
--- watershed-method/m-2.py
+++ watershed-method/m-2.py
@@ -4,7 +4,7 @@ import numpy as np
 from random import randint  # for random values
 import copy  # for deepcopy on images
 
-IMG_PATH = r"C:\Users\hasan\Desktop\leaves-segmentation\method-2\test2.png"
+IMG_PATH = r"C:\Users\hasan\Desktop\leaves-segmentation\watershed-method\test4.png"
 img = cv.imread(IMG_PATH)
 cv.imshow("org", img)
 
@@ -14,14 +14,14 @@ cv.imshow("org", img)
 
 #blur image
 blur = cv.GaussianBlur(src= img, ksize=(7,7), borderType=cv.BORDER_DEFAULT,  sigmaX= 1.5, sigmaY=1.5)
-cv.imshow("5-blur", blur)
+#cv.imshow("5-blur", blur)
 
 """ #smooth image
 kernel = np.ones((5,5), dtype=np.float32) / 25
 smooth = cv.filter2D(blur, -1, kernel )
  """
 #convert hsv
-hsv_img = cv.cvtColor(blur, cv.COLOR_BGR2HSV)
+hsv_img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 
 # hsv filtering
 high_value = (30,110,130)
@@ -32,14 +32,17 @@ cv.imshow("image_threshold-hsv",image_threshold)
 #opening and dilate image
 kernel  = np.ones((3,3), np.uint8)
 opening = cv.morphologyEx(image_threshold, cv.MORPH_OPEN, kernel ,iterations= 4)
-sure_bg = cv.dilate(opening, kernel, iterations=8)
+sure_bg = cv.dilate(opening, kernel, iterations=5)
+cv.imshow("backgraund", sure_bg)
 
 # Finding sure foreground area
 dist_transform = cv.distanceTransform(opening, cv.DIST_L2, 5)
-_, sure_fg = cv.threshold(dist_transform, 0.3*dist_transform.max(), 255, 0)
+_, sure_fg = cv.threshold(dist_transform, 0.25*dist_transform.max(), 255, 0)
+cv.imshow("foregraund", sure_fg)
 
 sure_fg = np.uint8(sure_fg)
 unknown = cv.subtract(sure_bg, sure_fg)
+cv.imshow("unknown", unknown)
 
 _, markers = cv.connectedComponents(sure_fg)
 markers = markers + 1
@@ -60,6 +63,6 @@ color= (255, 255, 255), thickness= 2, lineType= cv.LINE_AA, bottomLeftOrigin= Fa
 fontScale= 0.8)
 
 cv.imshow("result", img)
-cv.imshow("resulttt", mask)
+cv.imshow("mask", mask)
 cv.waitKey()
 cv.destroyAllWindows()
